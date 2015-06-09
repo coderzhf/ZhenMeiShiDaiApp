eda19ee7edac573f67d78d02f894d7e3011b7aad
zx <mac1@zxlmac1deMac-mini.local>
2015-06-09 08:58:53 +0800

    Initial Commit

---
+++ ZhenMeiShiDaiApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZhenMeiShiDaiApp
+//
+//  Created by zx on 15/6/9.
+//  Copyright (c) 2015年 zxl－mac1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
